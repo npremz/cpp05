f5c49ed17fd501233cd36c35b2aaa4a196e7dfa3
Nicolas Premont <ni.premont@gmail.com>
2024-12-13 17:30:04 +0100

    waf

---
+++ ex00/srcs/Bureaucrat.d
@@ -0,0 +1,2 @@
+srcs/Bureaucrat.o: srcs/Bureaucrat.cpp srcs/../includes/Bureaucrat.hpp
+srcs/../includes/Bureaucrat.hpp:
---
+++ ex01/srcs/main.d
@@ -0,0 +1,2 @@
+srcs/main.o: srcs/main.cpp srcs/../includes/Bureaucrat.hpp
+srcs/../includes/Bureaucrat.hpp:
